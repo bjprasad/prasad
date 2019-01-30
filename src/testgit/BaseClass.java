b83b48835a67bca8f8ee3e35b20ba26075579f60
bjprasad <jagadishwara.bandaru@gmail.com>
2019-01-30 20:00:28 +0530

    created base class

---
+++ src/testgit/BaseClass.java
@@ -0,0 +1,12 @@
+package testgit;
+
+public class BaseClass {
+	public static void main(String[] args) {
+		baseClassmethod();
+	}
+	
+	public static void baseClassmethod() {
+		// TODO Auto-generated method stub
+		System.out.println("this is my base class");
+	}
+}
